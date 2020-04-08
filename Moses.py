103f78530baf33f94b45fd2f02a61373a60c685a
mostafa <mostafakhoshhal@yahoo.com>
2020-04-08 14:47:32 +0430

    change the python code

--- Moses.py
+++ Moses.py
@@ -3,8 +3,6 @@ def factorial(n):
 		return n
 	else:
 		return n * factorial(n-1)
-<<<<<<< HEAD
+
 print(factorial(7))
-=======
-print(factorial(5))
->>>>>>> d2a09207d16a5ac076d601610049c60202d604a5
+
